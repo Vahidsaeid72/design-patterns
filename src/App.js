aa0876ccfb0b20a38309ddc54160204cc312d2d6
vahid <@Vahidsaeid72>
2024-04-29 08:54:18 +0330

    split screen pattern

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import SplitScreen from './layoutComponent/splitScreen/SplitScreen';
+
+const LeftSideComponent = ()=>{
+  return<>
+    <div style={{background:"red"}}>this is left component</div>
+  </>
+}
+const RightSideComponent = ()=>{
+  return<>
+    <div style={{background:"blue"}}>this is right component</div>
+  </>
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <SplitScreen Left={LeftSideComponent} Right={RightSideComponent}/>
     </div>
   );
 }
