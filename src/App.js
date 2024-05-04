0a2aecff45fd91979dcbfde2e335393092b7a9f6
vahid <@Vahidsaeid72>
2024-05-04 13:20:25 +0330

    container component pattern - current user loader

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
-import './App.css';
-import List from './layoutComponent/lists/List';
-import LargeAuthorListItems from './layoutComponent/lists/authers/LargeListItems';
-import SmallAutherListItems from './layoutComponent/lists/authers/SmallListItems';
-import LargeBookListItems from './layoutComponent/lists/books/LargeListItems';
-import SmallBookListItems from './layoutComponent/lists/books/SmallListItems';
-import { authors } from './layoutComponent/lists/data/authors';
-import { books } from './layoutComponent/lists/data/books';
-import Modal from './layoutComponent/modals/Modal';
-import SplitScreen from './layoutComponent/splitScreen/SplitScreen';
+import './App.css'
+import CurrentUserLoader from './ContainerComponents/CurrentUserLoader.jsx';
+import UserInfo from './ContainerComponents/UserInfo';
 
 
 function App() {
-  return (<>
-      <Modal>
-        <div>this is my moadal</div>
-      </Modal>
-  </>);
+  return (
+      <CurrentUserLoader>
+        <UserInfo/>
+      </CurrentUserLoader>
+  );
 }
 
 export default App;
